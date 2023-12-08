dc6a43cc31d86e405850166e33fa39d3935f962f
Anton Zinchenko <AntonZinch3nko@yandex.ru>
2023-12-08 19:42:55 +0500

    [main] upd

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,28 @@ import './App.css';
 import ImageGrid from './components/ImageSlider/ImageGrid';
 
 function App() {
-  const images = [
-    process.env.NODE_ENV === 'development'
-      ? '/img/deck/Chariot.png'
-      : 'https://antonzinch3nko.github.io/taro-demo.github.io/img/deck/Chariot.png',
-    process.env.NODE_ENV === 'development'
-      ? '/img/deck/Lovers.png'
-      : 'https://antonzinch3nko.github.io/taro-demo.github.io/img/deck/Lovers.png',
-    process.env.NODE_ENV === 'development'
-      ? '/img/deck/Fool.png'
-      : 'https://antonzinch3nko.github.io/taro-demo.github.io/img/deck/Fool.png',
-  ];
+    const images = [
+        process.env.NODE_ENV === 'development'
+            ? '/img/deck/Chariot.png'
+            : 'https://antonzinch3nko.github.io/taro-demo.github.io/img/deck/Chariot.png',
+        process.env.NODE_ENV === 'development'
+            ? '/img/deck/Fool.png'
+            : 'https://antonzinch3nko.github.io/taro-demo.github.io/img/deck/Fool.png',
+        process.env.NODE_ENV === 'development'
+            ? '/img/deck/Lovers.png'
+            : 'https://antonzinch3nko.github.io/taro-demo.github.io/img/deck/Lovers.png',
+        process.env.NODE_ENV === 'development'
+            ? '/img/deck/Hunged_man.png'
+            : 'https://antonzinch3nko.github.io/taro-demo.github.io/img/deck/Hunged_man.png',
+    ];
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <ImageGrid images={images} />
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <ImageGrid images={images} />
+            </header>
+        </div>
+    );
 }
 
 export default App;
