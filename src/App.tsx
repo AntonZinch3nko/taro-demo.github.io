821f49536a5a63e28881328cf295dcced0aac605
Anton Zinchenko <AntonZinch3nko@yandex.ru>
2023-12-08 19:01:06 +0500

    [main] upd

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import ImageGrid from './components/ImageSlider/ImageGrid';
 
 function App() {
   const images = [
-    '/img/deck/Chariot.png', 
-    '/img/deck/Fool.png',
+    process.env.NODE_ENV === 'development'
+      ? '/img/deck/Chariot.png'
+      : 'https://antonzinch3nko.github.io/taro-demo.github.io/img/deck/Chariot.png',
+    process.env.NODE_ENV === 'development'
+      ? '/img/deck/Lovers.png'
+      : 'https://antonzinch3nko.github.io/taro-demo.github.io/img/deck/Lovers.png',
+    process.env.NODE_ENV === 'development'
+      ? '/img/deck/Fool.png'
+      : 'https://antonzinch3nko.github.io/taro-demo.github.io/img/deck/Fool.png',
   ];
 
   return (
