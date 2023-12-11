2e32fe7f4adc40aef4c8555d2425a7aa17b4cfd8
Anton Zinchenko <AntonZinch3nko@yandex.ru>
2023-12-11 11:48:18 +0500

    [master] upd

--- src/ui/Chakra.tsx
+++ src/ui/Chakra.tsx
@@ -2,34 +2,41 @@ import { ChakraProvider, ColorModeProvider, CSSReset } from '@chakra-ui/react';
 import { extendTheme } from '@chakra-ui/react';
 
 const customTheme = extendTheme({
+  styles: {
+    global: {
+      // 'body': {
+      //   backgroundColor: "red", // ваше обычное глобальное стилизование для body
+      // },
+      '::-webkit-scrollbar': {
+        width: '4px',
+      },
+      '::-webkit-scrollbar-track': {
+        width: '6px',
+      },
+      '::-webkit-scrollbar-thumb': {
+        background: 'gray',
+        borderRadius: '24px',
+      },
+    },
+  },
   config: {
-    initialColorMode: 'dark', // Set the initial color mode to dark
-    useSystemColorMode: false, // Optionally, you can set this to true to use the system color mode
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
   },
   colors: {
-    // Define your color palette here
     brand: {
-      50: '#f5f5f5', // Light shade
-      900: '#1a202c', // Dark shade
-      // ... more shades
+      50: '#f5f5f5',
+      900: '#1a202c',
     },
   },
-  components: {
-    // Customize specific components
-    Button: {
-      // Setting the default color scheme for Button
-      // defaultProps: {
-      //   colorScheme: 'brand',
-      // },
-    },
-    // ... other components
-  },
+  // ... другие компоненты
 });
+
 export const Chakra = ({ children }: { children: React.ReactNode }) => (
-  <ChakraProvider theme={customTheme}>
-    <ColorModeProvider>
-      <CSSReset />
-      {children}
-    </ColorModeProvider>
-  </ChakraProvider>
+    <ChakraProvider theme={customTheme}>
+        <ColorModeProvider>
+            <CSSReset />
+            {children}
+        </ColorModeProvider>
+    </ChakraProvider>
 );
